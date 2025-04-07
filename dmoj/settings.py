5c52b3f4263eef1658ab27071d548ef74d025a4c
HNgoc <lmtcv1.9@gmail.com>
2025-04-07 17:22:17 +0700

    a

--- dmoj/settings.py
+++ dmoj/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '5*9f5q57mqmlz2#f$x1h76&jxy#yortjl1v+l*6hd18$d*yx#0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['oj-1.onrender.com']
 
 CSRF_FAILURE_VIEW = 'judge.views.widgets.csrf_failure'
 
--- gunicorn_start.sh
+++ gunicorn_start.sh
@@ -3,5 +3,8 @@
 # Set the DJANGO_SETTINGS_MODULE environment variable
 export DJANGO_SETTINGS_MODULE="dmoj.settings"
 
+# Collect static files before starting Gunicorn
+python manage.py collectstatic --noinput
+
 # Start Gunicorn
 gunicorn dmoj.wsgi:application --bind 0.0.0.0:10000
