f8daec092da38f65c8bf57f6f6115e2a8edd97dc
HNgoc <144669382+hngoc-29@users.noreply.github.com>
2025-04-11 08:35:26 +0000

    up

--- judge/admin/taxon.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-
-class ProblemGroupAdmin(admin.ModelAdmin):
-    fields = ('name', 'full_name')
-
-
-class ProblemTypeAdmin(admin.ModelAdmin):
-    fields = ('name', 'full_name')
--- judge/widgets/__init__.py
+++
@@ -1,4 +0,0 @@
-from judge.widgets.checkbox import CheckboxSelectMultipleWithSelectAll
-from judge.widgets.martor import *
-from judge.widgets.mixins import CompressorWidgetMixin
-from judge.widgets.select2 import *
