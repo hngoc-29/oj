959c62c953749d84af3636d10c4bd9bd99bf3874
HNgoc <144669382+hngoc-29@users.noreply.github.com>
2025-04-11 21:37:10 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
-supervisorctl -c conf/supervisord.conf status
 
+echo "🚀 Khởi động supervisord..."
+./vnojsite/bin/supervisord -c conf/supervisord.conf
+./vnojsite/bin/supervisorctl -c conf/supervisord.conf s
 echo "🚀 Khởi động nginx..."
 nginx -t
 nginx -c /workspaces/vnoj/conf/nginx.conf -g 'daemon off;'
